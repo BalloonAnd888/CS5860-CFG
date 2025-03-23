79e8d5ce236c889a6578a0008fce5a61a4b8e22c
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2025-03-22 17:10:23 -0700

    if statement

--- main.py
+++ main.py
@@ -2,34 +2,70 @@ class Node:
     def __init__(self, nodeID, statement):
         self.nodeID = nodeID
         self.statement = statement
+        self.edges = []
+
+def handleIf(lines, i, nodes, nodeID, edges):
+    ifNode = Node(nodeID, lines[i])
+    nodes.append(Node(nodeID, lines[i]))
+    nodeID += 1
+    i += 1
+    print(i, nodeID)
+
+    while lines[i] != "}" and i < len(lines):
+        if lines[i] != "{":
+            node = Node(nodeID, lines[i])
+            nodes.append(node)
+            edges.append((nodes[-2].nodeID, nodeID))
+            print(node.nodeID, node.statement)
+            print(edges)
+            nodeID += 1
+        i += 1
+
+    if i + 1 < len(lines):
+        edges.append((ifNode.nodeID, nodeID))
+
+
+    return i, nodeID
 
 def cfg(filename):
+    # Lines of code
     lines = []
     with open(filename, 'r') as file:
         for line in file:
             if line != "\n":
-                lines.append(line.rstrip())
+                lines.append(line.rstrip().lstrip())
     
     print(lines)
 
+    # Create nodes
     nodes = []
     nodeID = 1
-    for line in lines:
-        nodes.append(Node(nodeID, line.rstrip()))
-        nodeID += 1
 
+    edges = []
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("if"):
+            print(lines[i], "If")
+            if nodes:
+                edges.append((nodes[-1].nodeID, nodeID))
+                print(edges)
+            i, nodeID = handleIf(lines, i, nodes, nodeID, edges) # i = 3, nodeID = 4
+            print("After handleIF", i, nodeID)
+        elif lines[i] not in {"{", "}"}:
+            print(lines[i], "Statement")
+            node = Node(nodeID, lines[i])
+            print(node.nodeID, node.statement)
+            nodes.append(node)
+            if len(nodes) > 1:
+                edges.append((nodes[-2].nodeID, nodes[-1].nodeID))
+                print(edges)
+            nodeID += 1
+
+        i += 1
+
+    print("\nVertices (Nodes):")
     for n in nodes:
-        print(n.nodeID, n.statement)
-
-    vertices = [node.nodeID for node in nodes]
-    edges = [(nodes[i].nodeID, nodes[i+1].nodeID) for i in range(len(nodes) - 1)]
-
-    print("Vertices:", vertices)
-    print("Edges:", edges)
-
-    print("Vertices (Nodes):")
-    for node in nodes:
-        print(f"({node.nodeID}) {node.statement}")
+        print(f"({n.nodeID}) {n.statement}")
 
     print("\nEdges (Connections):")
     for edge in edges:
@@ -39,9 +75,6 @@ def cfg(filename):
 if __name__ == "__main__":
     print("Statement")
     cfg("statement.txt")
-    print("\nif")
+    print("if")
     cfg("if.txt")
-
-
-
-
+        
