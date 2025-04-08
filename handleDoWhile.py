5d10338aaa8d424b8c26fc86137cf2ad13692ba7
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2025-04-07 20:46:11 -0700

    Nested Do While

--- handleDoWhile.py
+++ handleDoWhile.py
@@ -1,6 +1,10 @@
 from node import Node 
 
 def handleDoWhile(lines, i, nodes, nodeID, edges, nodesToConnect):
+    from handleIf import handleIf
+    from handleWhile import handleWhile
+    from handleFor import handleFor
+
     i += 2
 
     doFirstNode = Node(nodeID, lines[i])
@@ -20,14 +24,32 @@ def handleDoWhile(lines, i, nodes, nodeID, edges, nodesToConnect):
 
     while lines[i] != "}" and i < len(lines):
         if lines[i] != "{":
-            node = Node(nodeID, lines[i])
-            nodes.append(node)
-            edges.append((nodesToConnect[-1].nodeID, node.nodeID))
-            nodesToConnect.pop()
-            nodesToConnect.append(node)
-            print(node.nodeID, node.statement)
-            print(edges)
-            nodeID += 1
+            firstWord = lines[i].split()[0]
+            if firstWord == "if":
+                i, nodeID, nodesToConnect = handleIf(lines, i, nodes, nodeID, edges, nodesToConnect)
+            elif firstWord == "while":
+                print(lines[i], "While")
+                i, nodeID, nodesToConnect, lastNode = handleWhile(lines, i, nodes, nodeID, edges, nodesToConnect)
+                nodesToConnect.append(lastNode)
+                print("LastNode ID", lastNode.nodeID)
+            elif firstWord == "for":
+                print(lines[i], "For")
+                i, nodeID, nodesToConnect, lastNode = handleFor(lines, i, nodes, nodeID, edges, nodesToConnect)
+                nodesToConnect.append(lastNode)
+                print("LastNode ID", lastNode.nodeID)
+            elif firstWord == "do":
+                print(lines[i], "Do")
+                i, nodeID, nodesToConnect = handleDoWhile(lines, i, nodes, nodeID, edges, nodesToConnect)
+                print("After handleDoWhile", i, nodeID)
+            else:
+                node = Node(nodeID, lines[i])
+                nodes.append(node)
+                edges.append((nodesToConnect[-1].nodeID, node.nodeID))
+                nodesToConnect.pop()
+                nodesToConnect.append(node)
+                print(node.nodeID, node.statement)
+                print(edges)
+                nodeID += 1
         i += 1
 
     i += 1
--- tests/testCFGDoWhile.py
+++ tests/testCFGDoWhile.py
@@ -23,12 +23,40 @@ class TestCFGIDoWhileStatement(unittest.TestCase):
             (6, 7) 
         ]
 
-    def testCFGDoWhileStatement(self):
+        self.nestedNodes = [
+            "(1) int x = 0;",
+            "(2) x = x + 1;",
+            "(3) x = x + 2;",
+            "(4) while (x < 5);",
+            "(5) x = x - 1;",
+            "(6) while (x < 10);",
+            "(7) x = x * 2;"
+        ]
+
+        self.nestedEdges = [
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 3),
+            (4, 5),
+            (5, 6),
+            (6, 2),
+            (6, 7)
+        ]
+
+    def testSimpleDoWhileStatement(self):
         self.testNodes, self.testEdges = cfg("examples/doWhile/doWhile.txt")
         self.assertEqual(self.edges, self.testEdges)
 
         for i, node in enumerate(self.testNodes):
             self.assertEqual(f"({node.nodeID}) {node.statement}", self.nodes[i])
+    
+    def testNestedDoWhile(self):
+        self.testNodes, self.testEdges = cfg("examples/doWhile/doWhileNested.txt")
+        self.assertEqual(sorted(self.testEdges), sorted(self.nestedEdges))
+        for i, node in enumerate(self.testNodes):
+            self.assertEqual(f"({node.nodeID}) {node.statement}", self.nestedNodes[i])
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
