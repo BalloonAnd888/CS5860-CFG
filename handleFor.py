1c41962be6e3383315b9720199622cfe0081edff
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2025-04-08 21:05:37 -0700

    Fix for

--- handleFor.py
+++ handleFor.py
@@ -23,19 +23,25 @@ def handleFor(lines: list, i: int, nodes: list, nodeID: int, edges: list, nodesT
         if lines[i] != "{":
             firstWord = lines[i].split()[0]
             if firstWord == "if":
+                print(lines[i], "If")
                 i, nodeID, nodesToConnect = handleIf(lines, i, nodes, nodeID, edges, nodesToConnect)
+                print("After handleIf", i, nodeID)
             elif firstWord == "while":
                 print(lines[i], "While")
                 i, nodeID, nodesToConnect, lastNode = handleWhile(lines, i, nodes, nodeID, edges, nodesToConnect)
                 nodesToConnect.append(lastNode)
-                print("LastNode ID", lastNode.nodeID)
+                print("Last Node", lastNode.nodeID)
+                print("After handleWhile", i, nodeID)
             elif firstWord == "for":
                 print(lines[i], "For")
                 i, nodeID, nodesToConnect, lastNode = handleFor(lines, i, nodes, nodeID, edges, nodesToConnect)
                 nodesToConnect.append(lastNode)
-                print("LastNode ID", lastNode.nodeID)
+                print("Last Node", lastNode.nodeID)
+                print("After handleFor", i, nodeID)
             elif firstWord == "do":
+                print(lines[i], "Do")
                 i, nodeID, nodesToConnect = handleDoWhile(lines, i, nodes, nodeID, edges, nodesToConnect)
+                print("After handleDoWhile", i, nodeID)
             else:
                 node = Node(nodeID, lines[i])
                 nodes.append(node)
--- tests/testCFGFor.py
+++ tests/testCFGFor.py
@@ -42,6 +42,42 @@ class TestCFGIForStatement(unittest.TestCase):
             (2, 7)
         ]
 
+        self.forWithAllNestedNodes = [
+            "(1) int x = 0;",
+            "(2) for (int i = 0; i < 3; i++)",
+            "(3) x = x + 1;",
+            "(4) while (x < 5)",
+            "(5) x = x + 2;",
+            "(6) for (int j = 0; j < 2; j++)",
+            "(7) x = x * 2;",
+            "(8) x = x - 1;",
+            "(9) while (x > 0);",
+            "(10) if (x == 0)",
+            "(11) x = x + 100;",
+            "(12) x++;",
+            "(13) x = x - 3;"
+        ]
+
+        self.forWithAllNestedEdges = [
+            (1, 2),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+            (5, 4),
+            (4, 6),
+            (6, 7),
+            (7, 6),
+            (6, 8),
+            (8, 9),
+            (9, 8),
+            (9, 10),
+            (10, 11),
+            (10, 12),
+            (11, 12),
+            (12, 2),
+            (2, 13)
+        ]
+
     def testSimpleFor(self):
         self.testNodes, self.testEdges = cfg("examples/for/for.txt")
         self.assertEqual(self.edges, self.testEdges)
@@ -54,6 +90,12 @@ class TestCFGIForStatement(unittest.TestCase):
         self.assertEqual(sorted(testEdges), sorted(self.nestedEdges))
         for i, node in enumerate(testNodes):
             self.assertEqual(f"({node.nodeID}) {node.statement}", self.nestedNodes[i])
+
+    def testForWithAllNested(self):
+        testNodes, testEdges = cfg("examples/for/forWithOther.txt")
+        self.assertEqual(sorted(testEdges), sorted(self.forWithAllNestedEdges))
+        for i, node in enumerate(testNodes):
+            self.assertEqual(f"({node.nodeID}) {node.statement}", self.forWithAllNestedNodes[i])
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
